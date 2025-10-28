f75805bbe01a340c8e66b019eb5a7bdfc7689121
adrianmendozatabaz <myamyamya29@gmail.com>
2025-10-27 22:33:48 -0600

    feat: add functionality in form, now is possible to save

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,20 +7,57 @@
         mensaje: ''
     })
 
-    const paciente = reactive({
-        nombre: '',
-        propietario: '',
-        email: '',
-        alta: '',
-        sintomas: '',
+    const emit = defineEmits(['update:nombre',
+        'update:propietario',
+        'update:email',
+        'update:alta',
+        'update:sintomas',
+        'guardar-paciente'
+    ])
+
+    const props = defineProps ({
+        nombre: {
+            type: String,
+            required: true
+        },
+        propietario: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        alta: {
+            type: String,
+            required: true
+        },
+        sintomas: {
+            type: String,
+            required: true
+        }
     })
 
     const validar = () => {
-         if(Object.values(paciente).includes('')) {
+         if(Object.values(props).includes('')) {
             alerta.mensaje = 'Todos los campos son obligatorios'
             alerta.tipo = 'error'
             return
-         }          
+         }
+         //* If the validaion is success emit = save
+         emit('guardar-paciente')
+
+         //* Show alert
+         alerta.mensaje = 'Guardado Correctamente'
+         alerta.tipo = "exito"
+
+         //* Clean the alert
+         setTimeout(() => {
+            Object.assign(alerta, {
+                tipo: '',
+                mensaje: ''
+            })
+         }, 3000);
     }
 
 </script>
@@ -31,6 +68,11 @@
             Añade pacientes y
             <span class="text-indigo-600 font-bold">Adminístralos</span>
         </p>
+
+        <Alerta
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        />
         
         <form action="#" 
                 class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
@@ -42,7 +84,8 @@
                         id="mascota" 
                         class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
                         placeholder="Ingresa el nombre de la mascota"
-                        v-model="paciente.nombre"
+                        :value="nombre"
+                        @input="$emit('update:nombre', $event.target.value)"
                 >
             </div>
 
@@ -52,7 +95,8 @@
                         id="propietario" 
                         class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
                         placeholder="Ingresa el nombre del propietario"
-                        v-model="paciente.propietario"
+                        :value="propietario"
+                        @input="$emit('update:propietario', $event.target.value)"
                 >
             </div>
 
@@ -62,7 +106,8 @@
                         id="email" 
                         class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
                         placeholder="Ingresa un correo electronico"
-                        v-model="paciente.email"
+                        :value="email"
+                        @input="$emit('update:email', $event.target.value)"
                 >
             </div>
 
@@ -71,7 +116,8 @@
                 <input type="date" 
                         id="alta" 
                         class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        v-model="paciente.alta"
+                        :value="alta"
+                        @input="$emit('update:alta', $event.target.value)"
                 >
             </div>
 
@@ -80,7 +126,8 @@
                 <textarea id="sintomas" 
                             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
                             placeholder="Ingrese los sintomas"
-                            v-model="paciente.sintomas"
+                            :value="sintomas"
+                            @input="$emit('update:sintomas', $event.target.value)"
                 ></textarea>
             </div>
 
